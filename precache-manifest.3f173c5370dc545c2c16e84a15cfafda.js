be04885c71bd29380d716d99af1fbefeba5de36a
David Ignacio <deignacio@gmail.com>
2020-02-09 15:09:32 -0800

    Updates

---
+++ precache-manifest.3f173c5370dc545c2c16e84a15cfafda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16fbc3f65db41b2996b78ed6ff5f3a8",
+    "url": "/walking-school-bus/index.html"
+  },
+  {
+    "revision": "0941fb1dfab0e682bfa6",
+    "url": "/walking-school-bus/static/css/main.511008c9.chunk.css"
+  },
+  {
+    "revision": "e8858702151df680be9f",
+    "url": "/walking-school-bus/static/js/2.e81e80b5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/walking-school-bus/static/js/2.e81e80b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0941fb1dfab0e682bfa6",
+    "url": "/walking-school-bus/static/js/main.16332540.chunk.js"
+  },
+  {
+    "revision": "616fba7eed6c053dd98b",
+    "url": "/walking-school-bus/static/js/runtime-main.b362ff41.js"
+  }
+]);
