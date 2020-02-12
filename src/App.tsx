9f70d324e16ad64c7ef17d5c53ddbee9e86c1ccb
David Ignacio <deignacio@gmail.com>
2020-02-12 01:57:19 -0800

    OMG AWS integration
    * Incremental run updates are sent do Dynamo via API Gateway, lambda
    * Others can view any in-progress (not ended) runs
    * Others can join and modify in-progress runs

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,172 @@
 import React from 'react';
 import './App.css';
-import { DateUtils, LocationUtils } from './Utils';
+import { DateUtils, LocationUtils, RunPersistence } from './Utils';
 
-import { SchoolModel, RouteModel, StopModel, RunModel } from './Models';
+import { SchoolModel, RouteModel, BusLeader, StopModel, RunModel, StopArrivalModel } from './Models';
 import { SchoolChooser, RouteChooser, LeaderChooser, LeaderChooserMode } from './Choosers';
 import { ArrivedStop, NextStop, PendingStop } from './Stops';
 import SCHOOLS from './Schools';
 
 type AppProps = {}
 
-type AppModel = {
-  run: RunModel
-}
+enum AppMode {
+  LEAD, WATCH
+};
 
-export default class App extends React.Component<AppProps, AppModel> {
+type AppState = RunModel & {
+  mode: AppMode,
+  previous: Array<RunModel>,
+  watch?: NodeJS.Timeout,
+};
 
-  constructor(props: AppProps, model: AppModel) {
-    super(props, model)
+const initialState = {
+  mode: AppMode.WATCH,
+  leaders: new Array<BusLeader>(),
+  arrivals: new Array<StopArrivalModel>(),
+  previous: new Array<RunModel>(),
+};
 
-    this.state = { run: { arrivals: [], leaders: [] } };
+export default class App extends React.Component<AppProps, AppState> {
+  readonly state: AppState = initialState;
+
+  componentDidMount() {
+    RunPersistence.listOpenRuns().then((previous: Array<RunModel>) => this.onPreviousRuns(previous));
+  }
+
+  componentWillUnmount() {
+    this.stopWatch();
+  }
+
+  private startWatch() {
+    if (this.state.watch === undefined) {
+      const watch = setInterval( () => {
+        if (this.state.mode === AppMode.LEAD) {
+          this.stopWatch();
+        } else {
+          RunPersistence.listOpenRuns().then((previous: Array<RunModel>) => this.onPreviousRuns(previous));
+        }
+      }, 15 * 1000);
+      this.setState({ watch: watch });
+    }
+  }
+
+  private stopWatch() {
+    if (this.state.watch) {
+      clearInterval(this.state.watch);
+      this.setState({ watch: undefined });
+    }
+  }
+
+  private async onPreviousRuns(previous: Array<RunModel>) {
+    this.setState({ previous: previous });
+  }
+
+  private async onPreviousJoin(run: RunModel) {
+    this.setState({ mode: AppMode.LEAD });
+    this.setState(run);
+    this.stopWatch();
+  }
+
+  private async onCreateNew() {
+    this.setState({ mode: AppMode.LEAD });
+    this.stopWatch();
+  }
+
+  private get previous(): any {
+    let previous: any = this.state.previous.map((run, i) => {
+      const leaders = run.leaders
+        .map((leader) => leader.name)
+        .join(', ');
+      const start = run.start?.toLocaleString();
+      const lastStop = run.arrivals[run.arrivals.length - 1]?.stop.name;
+      return (<div className="Previous-Run" key={i + ''}>
+        <div className="Previous-Name">{run.school?.name} - {run.route?.name}</div>
+        <div className="Previous-Start">{start}</div>
+        <div className="Previous-Leaders">{leaders}</div>
+        <div className="Previous-LastStop">Last Stop: {lastStop}</div>
+        <div className="Previous-Buttons">
+          <button onClick={() => this.onPreviousJoin(run)}>Join the Run</button> - <button onClick={() => this.startWatch()}>Watch for Updates</button>
+        </div>
+      </div>);
+    });
+    if (previous.length === 0) {
+      previous = 'None';
+    }
+    return (<div className="Previous">
+        <div className="Previous-Runs">
+          Open Runs: {previous}
+        </div>
+        <hr/>
+        <button onClick={() => this.onCreateNew()}>Create New Run</button>
+      </div>);
   }
 
   /** Async handler to record the school chosen in the run model */
   private async onSchoolChosen(school: SchoolModel): Promise<any> {
-    this.setState( (state, props) => {
-      state.run.school = school;
-      return state;
-    });
+    this.setState({ school: school });
   }
 
   /** Convenience method, either renders the school chooser, or the chosen school */
   private get school(): any {
-    if (!this.state.run.school) {
+    if (!this.state.school) {
       return (<SchoolChooser
         schools={SCHOOLS}
         callback={(school: SchoolModel) => this.onSchoolChosen(school)}
         />);
     }
-    return (<div className="Run-School">School: {this.state.run.school.name}</div>);
+    return (<div className="Run-School">School: {this.state.school.name}</div>);
   }
 
   /** Async handler to record the route chosen in the run model */
   private async onRouteChosen(route: RouteModel): Promise<any> {
-    this.setState( (state, props) => {
-      state.run.route = route;
-      return state;
-    });
+    this.setState({ route: route });
   }
 
   /** Convenience method, either renders the route chooser for the school or the chosen route */
   private get route(): any {
-    if (!this.state.run.school) {
+    if (!this.state.school) {
       return '';
-    } else if (!this.state.run.route) {
+    } else if (!this.state.route) {
       return (<RouteChooser
-        routes={this.state.run.school.routes}
+        routes={this.state.school.routes}
         callback={(route: RouteModel) => this.onRouteChosen(route)}
         />);
     }
-    return (<div className="Run-Route">Route: {this.state.run.route.name}</div>);
+    return (<div className="Run-Route">Route: {this.state.route.name}</div>);
   }
 
   /** Async handler to record that a leader has been specified, starting the run if necessary */
   private async onLeaderChosen(leader: string): Promise<any> {
+    this.setState( {
+      leaders: [{ name: leader }],
+      start: new Date(),
+      mode: AppMode.LEAD,
+    }, () => RunPersistence.emitRunUpdate(this.state));
+  }
+
+  private async onLeaderAdded(leader: string): Promise<any> {
     this.setState( (state, props) => {
-      state.run.leaders.push({ name: leader });
-      if (!state.run.start) {
-        state.run.start = new Date();
-      }
-      return state;
-    });
+      return { leaders: state.leaders.concat({ name: leader }) };
+    }, () => RunPersistence.emitRunUpdate(this.state));
+
   }
 
   /** Async handler to remove a leader from the run */
   private async onLeaderRemoved(leaderIndex: number): Promise<any> {
     this.setState( (state, props) => {
-      state.run.leaders = state.run.leaders.filter((_, index) => index !== leaderIndex);
-      return state;
-    });
+      return { leaders: state.leaders.filter((_, index) => index !== leaderIndex) };
+    }, () => RunPersistence.emitRunUpdate(this.state));
   }
 
   /** Convenience method, either renders the start route button or the current leader */
   private get leader(): any {
-    if (!this.state.run.school || !this.state.run.route) {
+    if (!this.state.school || !this.state.route) {
       return '';
-    } else if (this.state.run.leaders.length === 0) {
+    } else if (this.state.leaders.length === 0) {
       return (<LeaderChooser mode={LeaderChooserMode.First} callback={(leader:string) => this.onLeaderChosen(leader)}/>);
     }
-    const numLeaders = this.state.run.leaders.length;
-    const leaders: Array<any> = this.state.run.leaders.map((leader, i) => {
+    const numLeaders = this.state.leaders.length;
+    const leaders: Array<any> = this.state.leaders.map((leader, i) => {
       let removeButton: any = '';
       if (numLeaders > 1) {
         const index = i;
@@ -101,17 +179,17 @@ export default class App extends React.Component<AppProps, AppModel> {
     });
     return (<div className="Run-Leaders">
       Leaders: {leaders}
-      <LeaderChooser mode={LeaderChooserMode.Add} callback={(leader:string) => this.onLeaderChosen(leader)}/>
+      <LeaderChooser mode={LeaderChooserMode.Add} callback={(leader:string) => this.onLeaderAdded(leader)}/>
       </div>);
   }
 
   /** Convenience method, renders the start time of the run if it has started */
   private get start(): any {
-    if (!this.state.run.start) {
+    if (!this.state.start) {
       return '';
     }
 
-    return (<div className="Run-Start">Start: {this.state.run.start.toISOString()}</div>);
+    return (<div className="Run-Start">Start: {this.state.start.toLocaleString()}</div>);
   }
 
   /** Async handler, records an arrival at a stop in the run model */
@@ -119,41 +197,45 @@ export default class App extends React.Component<AppProps, AppModel> {
     const here = await LocationUtils.getCurrentPosition();
     this.setState( (state, props) => {
       const now = DateUtils.now();
-      state.run.arrivals.push({
-        stop: stop,
-        timestamp: now,
-        position: here,
-      });
-      if (state.run.route && state.run.arrivals.length === state.run.route.stops.length) {
-        state.run.end = now;
+      return {
+        arrivals: state.arrivals.concat({
+          stop: stop,
+          timestamp: now,
+          position: here,
+        })
+      };
+    }, () => {
+      if (this.state.route && this.state.arrivals.length === this.state.route.stops.length) {
+        this.setState({ end: DateUtils.now() }, () => RunPersistence.emitRunUpdate(this.state));
+      } else {
+        RunPersistence.emitRunUpdate(this.state);
       }
-      return state;
     });
   }
 
   /** Most of the magic is here, renders the different stops along the route with their current state */
   private get stops(): any {
-    if (!this.state.run.route || !this.state.run.start) {
+    if (!this.state.route || !this.state.start) {
       return '';
     }
     const elements: Array<any> = [];
     // Loop through all stops that we've already arrived at
     let i = 0;
-    for (; i < this.state.run.arrivals.length; i++) {
-      const arrival = this.state.run.arrivals[i];
+    for (; i < this.state.arrivals.length; i++) {
+      const arrival = this.state.arrivals[i];
       elements.push((<ArrivedStop arrival={arrival} key={i + ''}/>));
     }
 
     // Render the next stop along the route
-    const nextStop = this.state.run.route.stops[i];
+    const nextStop = this.state.route.stops[i];
     if (nextStop) {
       elements.push((<NextStop stop={nextStop} onArrival={(stop: StopModel) => this.onArrival(stop)} key={i + ''}/>));
       i++;
     }
 
     // Render any pending steps remaining in the route
-    while (i < this.state.run.route.stops.length) {
-      const stop = this.state.run.route.stops[i];
+    while (i < this.state.route.stops.length) {
+      const stop = this.state.route.stops[i];
       elements.push((<PendingStop stop={stop} key={i + ''}/>));
       i++;
     }
@@ -162,19 +244,19 @@ export default class App extends React.Component<AppProps, AppModel> {
 
   /** Convenience method, renders the end time of the run, if any */
   private get end(): any {
-    if (!this.state.run.end) {
+    if (!this.state.end) {
       return '';
      }
-    return (<div className="Run-End">End: {this.state.run.end.toISOString()}</div>);
+    return (<div className="Run-End">End: {this.state.end.toLocaleString()}</div>);
   }
 
   /** Triggers a file download of the current run's data */
   private async onDownloadPressed(): Promise<any> {
     const options: BlobPropertyBag = { type: 'application/json' };
-    const blob: Blob = new Blob([JSON.stringify(this.state.run)], options);
+    const blob: Blob = new Blob([JSON.stringify(this.state)], options);
     const element = document.createElement('a');
     element.setAttribute('href', window.URL.createObjectURL(blob));
-    const filename = this.state.run.school?.name + '-' + this.state.run.route?.name + '-' + this.state.run.start?.toISOString + '.json';
+    const filename = this.state.school?.name + '-' + this.state.route?.name + '-' + this.state.start?.toISOString() + '.json';
     element.setAttribute('download', filename);
     element.style.display = 'none';
     document.body.appendChild(element);
@@ -184,7 +266,7 @@ export default class App extends React.Component<AppProps, AppModel> {
 
   /** Convenience method, renders a download button if the run is done */
   private get download(): any {
-    if (!this.state.run.end) {
+    if (!this.state.end) {
       return '';
     }
 
@@ -192,20 +274,27 @@ export default class App extends React.Component<AppProps, AppModel> {
   }
 
   render() {
+    let body;
+    if (this.state.mode === AppMode.WATCH) {
+      body = this.previous;
+    } else if (this.state.mode === AppMode.LEAD) {
+      body = (<div className="Run">
+        {this.school}
+        {this.route}
+        {this.leader}
+        {this.start}
+        {this.stops}
+        {this.end}
+        {this.download}
+      </div>);
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <p>Walking School Bus</p>
         </header>
-        <div className="Run">
-          {this.school}
-          {this.route}
-          {this.leader}
-          {this.start}
-          {this.stops}
-          {this.end}
-          {this.download}
-        </div>
+        {body}
       </div>
     );
   }
--- src/Utils.tsx
+++ src/Utils.tsx
@@ -1,3 +1,5 @@
+import { RunModel } from './Models';
+
 /** Helper class to encapsulate Date methods */
 class DateUtils {
   /** Helper that returns the current date */
@@ -18,6 +20,40 @@ class DeveloperUtils {
     latitude: 5,
     longitude: 6,
     speed: 7
+  }
+
+  static FAKE_RUN: RunModel = {
+    school: {
+      name: 'Fake School',
+      routes: [{
+        name: 'Fake Route',
+        stops: [{
+          name: 'Fake Stop A'
+        }, {
+          name: 'Fake Stop B'
+        }]
+      }]
+    },
+    route: {
+      name: 'Fake Route',
+      stops: [{
+        name: 'Fake Stop A'
+      }, {
+        name: 'Fake Stop B'
+      }]
+    },
+    leaders: [{ name: 'me' }],
+    start: DateUtils.now(),
+    arrivals: [{
+      stop: {
+        name: 'Fake Stop A'
+      },
+      timestamp: DateUtils.now(),
+      position: {
+        coords: DeveloperUtils.FAKE_COORDINATES,
+        timestamp: DateUtils.now().getTime()
+      }
+    }]
   }
 
   public static isDeveloper(): boolean {
@@ -43,8 +79,7 @@ class LocationUtils {
       window.navigator.geolocation.getCurrentPosition((position: Position) => {
         resolve(position);
       }, (error: PositionError) => {
-        const isDeveloper = DeveloperUtils.isDeveloper();
-        if (isDeveloper) {
+        if (DeveloperUtils.isDeveloper()) {
           resolve(DeveloperUtils.fakePosition());
         } else {
           reject(error);
@@ -54,7 +89,79 @@ class LocationUtils {
   }
 }
 
+class RunPersistence {
+  private static ENDPOINT: string = 'https://7098mtfhad.execute-api.us-west-2.amazonaws.com/Prod/WalkingSchoolBusRun';
+  private static TABLE_NAME: string = 'walking-school-bus-runs';
+
+  public static async emitRunUpdate(run: RunModel): Promise<Response> {
+    run.arrivals = run.arrivals.map( (arrival) => {
+      const position = {
+        timestamp: arrival.position.timestamp,
+        coords: {
+          accuracy: arrival.position.coords.accuracy,
+          altitude: arrival.position.coords.altitude,
+          altitudeAccuracy: arrival.position.coords.altitudeAccuracy,
+          heading: arrival.position.coords.heading,
+          latitude: arrival.position.coords.latitude,
+          longitude: arrival.position.coords.longitude,
+          speed: arrival.position.coords.speed
+        }
+      }
+      arrival.position = position
+      return arrival;
+    })
+    const body: string = JSON.stringify({
+      TableName: 'walking-school-bus-runs',
+      Item: {
+        school_route: run.school?.name + '_' + run.route?.name,
+        start: run.start,
+        run: run
+      }
+    });
+    return fetch(RunPersistence.ENDPOINT, {
+      method: 'POST',
+      body: body,
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  public static async listOpenRuns(): Promise<Array<RunModel>> {
+    return fetch(`${RunPersistence.ENDPOINT}?TableName=${RunPersistence.TABLE_NAME}`, {
+        method: 'GET',
+        mode: 'cors'
+      })
+      .then((response: Response) => response.json())
+      .catch((reason) => {
+        console.log('Catching listOpenRuns: ' + reason);
+        if (DeveloperUtils.isDeveloper()) {
+          return JSON.parse('{"Items":[{"run":{"mode":0,"route":{"name":"Jefferson","stops":[{"name":"20th Ave and East Jefferson St"},{"name":"12th Ave and East Jefferson St"},{"name":"12th Ave and East Alder St"},{"name":"School"}]},"previous":[],"arrivals":[{"stop":{"name":"20th Ave and East Jefferson St"},"timestamp":"2020-02-12T08:29:57.044Z","position":{}}],"school":{"name":"Bailey Gatzert","routes":[{"name":"Jefferson","stops":[{"name":"20th Ave and East Jefferson St"},{"name":"12th Ave and East Jefferson St"},{"name":"12th Ave and East Alder St"},{"name":"School"}]},{"name":"Yesler","stops":[{"name":"19th Ave and East Yesler Way"},{"name":"17th Ave and East Yesler Way"},{"name":"15th Ave and East Yesler Way"},{"name":"School"}]}]},"start":"2020-02-12T08:29:54.289Z","leaders":[{"name":"Dave"}]},"school_route":"Bailey Gatzert_Jefferson","start":"2020-02-12T08:29:54.289Z"},{"run":{"mode":0,"route":{"name":"Jefferson","stops":[{"name":"20th Ave and East Jefferson St"},{"name":"12th Ave and East Jefferson St"},{"name":"12th Ave and East Alder St"},{"name":"School"}]},"previous":[],"arrivals":[],"school":{"name":"Bailey Gatzert","routes":[{"name":"Jefferson","stops":[{"name":"20th Ave and East Jefferson St"},{"name":"12th Ave and East Jefferson St"},{"name":"12th Ave and East Alder St"},{"name":"School"}]},{"name":"Yesler","stops":[{"name":"19th Ave and East Yesler Way"},{"name":"17th Ave and East Yesler Way"},{"name":"15th Ave and East Yesler Way"},{"name":"School"}]}]},"start":"2020-02-12T08:56:13.536Z","leaders":[{"name":"Anonymous"}]},"school_route":"Bailey Gatzert_Jefferson","start":"2020-02-12T08:56:13.536Z"}],"Count":2,"ScannedCount":2}');
+          // return [DeveloperUtils.FAKE_RUN];
+        } else {
+          throw reason;
+        }
+      })
+      .then((responseJSON) => responseJSON.Items)
+      .then((items) => items.map((item: any) => item.run))
+      .then((runs) => {
+        return runs.map((run: any) => {
+          run.start = new Date(Date.parse(run.start));
+          run.previous = [];
+          run.arrivals = run.arrivals.map((arrival: any) => {
+            arrival.timestamp = new Date(Date.parse(arrival.timestamp));
+            return arrival;
+          })
+          return run;
+        })
+        .filter((run: RunModel) => !run.end);
+      });
+  }
+}
+
 export {
   DateUtils,
-  LocationUtils
+  LocationUtils,
+  RunPersistence,
 }
